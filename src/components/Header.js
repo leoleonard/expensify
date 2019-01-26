daa7f210d92a7931914f9f82991b52b3d4883c03
leoleonard <damian.juzyk@gmail.com>
2019-01-27 00:00:10 +0100

    add specf for edit route

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,8 @@ const Header = () => (
     <header>
     <h1>Expensify</h1>
     <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink> 
-    <NavLink to="/edit" activeClassName="is-active"> Edit</NavLink> 
-    <NavLink to="/help" activeClassName="is-active"> Help</NavLink> 
     <NavLink to="/create" activeClassName="is-active"> Create</NavLink>
+    <NavLink to="/help" activeClassName="is-active"> Help</NavLink> 
     </header>
 );
 
