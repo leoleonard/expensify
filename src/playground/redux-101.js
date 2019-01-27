06046595809d0a14895bde1de353a7327e9c8958
leoleonard <damian.juzyk@gmail.com>
2019-01-27 11:56:17 +0100

    add store.dispatch

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,9 +1,51 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = {count: 10 }) => {
-    return state;
+const store = createStore((state = {count: 0 }, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+        return {
+            count: state.count + 1
+        };
+        case 'DECREMENT':
+        return {
+            count: state.count -1
+        }
+        case 'RESET':
+        return {
+            count: 0
+            }
+        default:
+            return state;
+    }
+});
+
+
+console.log(store.getState());
+
+// Actions
+
+// increment the count
+store.dispatch({
+    type: 'INCREMENT',
+});
+
+store.dispatch({
+    type: 'INCREMENT',
 });
 
 console.log(store.getState());
 
-console.log('hello hun');
+store.dispatch({
+    type: 'RESET',
+});
+
+console.log(store.getState());
+
+
+//decrement the count
+store.dispatch({
+    type: 'DECREMENT',
+});
+
+console.log(store.getState());
+
