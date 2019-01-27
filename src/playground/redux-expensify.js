8a5a45fd1cc80d24b022d05654b8b1c98c4d61c8
leoleonard <damian.juzyk@gmail.com>
2019-01-27 22:34:15 +0100

    add expense and filters reducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,55 @@
 import { createStore, combineReducers } from 'redux';
 
+// ADD_EXPENSE
+// REMOVE_EXPENSE
+// EDIT_EXPENSE
+
+// SET_TEXT_FILTER
+// SORT_BY_DATE
+// SORT_BY_AMOUNT
+// SET_START_DATE
+// SET_END_DATE
+
+// Expenses Reducer
+
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+    switch (action) {
+        default:
+        return state;
+    }
+};
+
+// Filers Reducer
+
+const filtersReducerDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+};
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+    switch (action) {
+        default:
+        return state;
+    }
+};
+
+// Store creation
+
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filters: filtersReducer
+    })
+    );
+
+console.log(store.getState());
+
+
+
 const demoState = {
     expenses: [{
         id: 'asdfg',
@@ -10,7 +60,7 @@ const demoState = {
     }],
     filters: {
         text: 'rent',
-        sortBy: 'amount', //date or amount
+        sortBy: 'amount',
         startDate: undefined,
         endDate: undefined
     }
