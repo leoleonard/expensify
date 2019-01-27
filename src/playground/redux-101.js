17544b8ba9b648b37c4f86cce1aa2ca71698f920
leoleonard <damian.juzyk@gmail.com>
2019-01-27 19:58:40 +0100

    add switch to redux

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,49 +3,56 @@ import { createStore } from 'redux';
 const store = createStore((state = {count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
+        const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
         return {
-            count: state.count + 1
+            count: state.count + incrementBy
         };
         case 'DECREMENT':
+        const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
         return {
-            count: state.count -1
+            count: state.count - decrementBy
         }
         case 'RESET':
         return {
             count: 0
             }
+        case 'SET':
+        return {
+            count: 69
+        }
         default:
             return state;
     }
 });
 
-
-console.log(store.getState());
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+});
 
 // Actions
-
-// increment the count
 store.dispatch({
     type: 'INCREMENT',
 });
 
 store.dispatch({
     type: 'INCREMENT',
+    incrementBy: 5
 });
-
-console.log(store.getState());
 
 store.dispatch({
     type: 'RESET',
 });
-
-console.log(store.getState());
-
-
-//decrement the count
+ 
 store.dispatch({
     type: 'DECREMENT',
+    decrementBy: 7
 });
 
-console.log(store.getState());
+store.dispatch({
+    type: 'DECREMENT'
+});
 
+store.dispatch({
+    type: 'SET',
+    count: 69
+});
