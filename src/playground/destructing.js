12b855300f95a07f17c854f19e4bead9ef236fdd
leoleonard <damian.juzyk@gmail.com>
2019-01-27 20:35:38 +0100

    add array destructing

--- src/playground/destructing.js
+++ src/playground/destructing.js
@@ -1,19 +1,27 @@
-const person = {
-    name: 'Damian',
-    age: 28,
-    location: {
-        city: 'Wrocław',
-        temp: 6
-    }
-}
+// const person = {
+//     name: 'Damian',
+//     age: 28,
+//     location: {
+//         city: 'Wrocław',
+//         temp: 6
+//     }
+// }
 
-const { name = 'anynomous', age } = person;
+// const { name = 'anynomous', age } = person;
 
-// const name = person.name;
-// const age = person.age;
+// // const name = person.name;
+// // const age = person.age;
 
-console.log(`${name} is ${age}.`);
+// console.log(`${name} is ${age}.`);
 
-const {city, temp: temperature} = person.location;
+// const {city, temp: temperature} = person.location;
 
-console.log(city, temperature);
+// console.log(city, temperature);
+
+ const address = ['Rymarska', 'Wroclaw', 'Poland'];
+
+ console.log(`You are on ${address[0]} in ${address[2]}`);
+
+ const [street, city, country] = address;
+
+ console.log(street);
