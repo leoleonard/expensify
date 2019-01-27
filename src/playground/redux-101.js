60ed78b1c66ed3f450d203714d7eb98403c15b40
leoleonard <damian.juzyk@gmail.com>
2019-01-27 22:02:43 +0100

    add reducer

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -27,15 +27,15 @@ const resetCount = () => ({
     type: 'RESET'
 });
 
-const store = createStore((state = {count: 0 }, action) => {
+// Reducers
+
+const countReducer = (state = {count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
-        // const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
         return {
             count: state.count + action.incrementBy
         };
         case 'DECREMENT':
-        // const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
         return {
             count: state.count - action.decrementBy
         }
@@ -50,7 +50,9 @@ const store = createStore((state = {count: 0 }, action) => {
         default:
             return state;
     }
-});
+};
+
+const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe(() => {
     console.log(store.getState());
