1428b641f9466ccfb28fd148a0d4edd3b82cec57
leoleonard <damian.juzyk@gmail.com>
2019-01-28 22:53:44 +0100

    add organizining redux before connecting to redux

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routes/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter} from './actions/filters';
+import getVisibleExpsenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+
+store.dispatch(addExpense({description: 'Water bill'}));
+store.dispatch(addExpense({description: 'Gas bill'}));
+store.dispatch(setTextFilter('water'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpsenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+// console.log(store.getState());
+
 
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
