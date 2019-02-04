729420c98bae8d5e0e128b4c52588aa6adf5eff3
leoleonard <damian.juzyk@gmail.com>
2019-02-04 23:44:39 +0100

    add refactoring of editExpensePage

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -6,7 +6,7 @@ import { addExpense } from '../actions/expenses';
 
 export class AddExpensePage extends React.Component {
     onSubmit= (expense) => {
-        this.props.onSubmit(expense);
+        this.props.addExpense(expense);
         this.props.history.push('/');
 };
 render() {
@@ -37,7 +37,7 @@ render() {
 // );
 
 const mapDispatchToProps = (dispatch) => ( {
-    onSubmit: (expense) => dispatch(addExpense(expense))
+    addExpense: (expense) => dispatch(addExpense(expense))
 });
 
 export default connect(undefined, mapDispatchToProps)(AddExpensePage);
